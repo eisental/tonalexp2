b359a567a961d50a77f4a0236bd9a212387e00dc
eisental <eisental@gmail.com>
2020-09-08 12:26:24 +0300

    Updates

---
+++ precache-manifest.3a4b60d60388f4092b1153d753fb3185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a4eea4630bdd63d7dd34f1b9f4956d",
+    "url": "/tonalexp/index.html"
+  },
+  {
+    "revision": "3463419e0c45a9754755",
+    "url": "/tonalexp/static/css/main.8ce88d2e.chunk.css"
+  },
+  {
+    "revision": "2d9e1f479598bd214748",
+    "url": "/tonalexp/static/js/2.cab310c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tonalexp/static/js/2.cab310c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3463419e0c45a9754755",
+    "url": "/tonalexp/static/js/main.997ca735.chunk.js"
+  },
+  {
+    "revision": "6970b1dd6fbbb060fbfa",
+    "url": "/tonalexp/static/js/runtime-main.4e0aa05d.js"
+  }
+]);
